8edf21e9c852a9aabfd3c85354976414d8d182d2
msmallest <85510853+msmallest@users.noreply.github.com>
2023-09-28 10:21:06 -0500

    1 file modified

--- src/app/map-observables/map-observables.component.ts
+++ src/app/map-observables/map-observables.component.ts
@@ -45,6 +45,8 @@ export class MapObservablesComponent implements OnInit {
     { firstName: 'Todd', lastName: 'Motto' },
   ]);
 
+  // comment to test adding something via stackblitz
+
   peopleWithFullName$ = this.people$.pipe(
     mergeMap((data) => data),
     map((person) => {
